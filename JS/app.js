2fc168b42895a742610df5f3a3d3b01b7268662e
Hixie4 <jhixie4@gmail.com>
2018-07-24 21:39:48 -0700

    object saving

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,6 @@
 
+function getLatestFixtures(){
+
 // set date to today'a date
 var todaysDateAndTimeObject = new Date();
 var todaysDateAndTime = todaysDateAndTimeObject.toISOString();
@@ -6,7 +8,7 @@ var todayDate = todaysDateAndTime.substring(0,10);
 console.log(todayDate);
 
 
-var apiStartString = "https://statsapi.web.nhl.com/api/v1/schedule?teamId=30&startDate=";
+var apiStartString = "https://statsapi.web.nhl.com/api/v1/schedule?teamId=23&startDate=";
 var apiEndString = "&endDate=2019-01-12";
 var concatApiString = apiStartString.concat(todayDate, apiEndString);
 console.log(concatApiString);
@@ -14,9 +16,9 @@ console.log(concatApiString);
 var nextGame = new XMLHttpRequest();
 nextGame.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-       
-
-
+        var nextGameObject = {};
+        nextGameObject = JSON.parse(nextGame.response);
+        debugger;
 
 
        document.getElementById("next-fixture").innerHTML = nextGame.responseText;
@@ -24,3 +26,5 @@ nextGame.onreadystatechange = function() {
 };
 nextGame.open("GET", concatApiString, true);
 nextGame.send();
+}
+getLatestFixtures();
