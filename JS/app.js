6ebebb1c7ce093171d0d7e0fe9d383ca75dad9b9
Hixie4 <jhixie4@gmail.com>
2018-07-24 21:26:53 -0700

    dynamic date added

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,26 @@
+
+// set date to today'a date
+var todaysDateAndTimeObject = new Date();
+var todaysDateAndTime = todaysDateAndTimeObject.toISOString();
+var todayDate = todaysDateAndTime.substring(0,10);
+console.log(todayDate);
+
+
+var apiStartString = "https://statsapi.web.nhl.com/api/v1/schedule?teamId=30&startDate=";
+var apiEndString = "&endDate=2019-01-12";
+var concatApiString = apiStartString.concat(todayDate, apiEndString);
+console.log(concatApiString);
+
 var nextGame = new XMLHttpRequest();
 nextGame.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        
+
+
+
+
        document.getElementById("next-fixture").innerHTML = nextGame.responseText;
     }
 };
-nextGame.open("GET", "https://statsapi.web.nhl.com/api/v1/schedule?teamId=30&startDate=2018-01-09&endDate=2018-01-12", true);
+nextGame.open("GET", concatApiString, true);
 nextGame.send();
