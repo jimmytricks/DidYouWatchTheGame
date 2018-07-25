4706315c7331210c8205efb395ca2373244f54c6
Hixie4 <jhixie4@gmail.com>
2018-07-24 21:42:12 -0700

    comments added

--- JS/app.js
+++ JS/app.js
@@ -3,11 +3,12 @@ function getLatestFixtures(){
 
 // set date to today'a date
 var todaysDateAndTimeObject = new Date();
+
+// save date in ISO format / makee it a string then cut out time part
 var todaysDateAndTime = todaysDateAndTimeObject.toISOString();
 var todayDate = todaysDateAndTime.substring(0,10);
-console.log(todayDate);
 
-
+// set vars to concat string based on todays date
 var apiStartString = "https://statsapi.web.nhl.com/api/v1/schedule?teamId=23&startDate=";
 var apiEndString = "&endDate=2019-01-12";
 var concatApiString = apiStartString.concat(todayDate, apiEndString);
@@ -16,6 +17,7 @@ console.log(concatApiString);
 var nextGame = new XMLHttpRequest();
 nextGame.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+        // parse response into empty object
         var nextGameObject = {};
         nextGameObject = JSON.parse(nextGame.response);
         debugger;
@@ -27,4 +29,5 @@ nextGame.onreadystatechange = function() {
 nextGame.open("GET", concatApiString, true);
 nextGame.send();
 }
+
 getLatestFixtures();
