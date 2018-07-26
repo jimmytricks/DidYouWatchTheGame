c1e11eb7b7502a80eafbaff90c466b0b7538567b
James <jhixie4@gmail.com>
2018-07-26 16:32:36 -0700

    outputting last fixtures before reverse

--- JS/app.js
+++ JS/app.js
@@ -65,9 +65,6 @@ function getLatestFixtures() {
             var game4 = nextGameObject.dates["4"].games["0"];
             var nextFourGames = [game1, game2, game3, game4];
             nextFourGames.forEach(createGameDate);
-
-            debugger;
-
         }
     };
     nextGame.open("GET", concatApiString, true);
@@ -75,3 +72,81 @@ function getLatestFixtures() {
 }
 
 getLatestFixtures();
+
+
+
+// last results
+
+
+function getLatestResults() {
+
+    // set date to today'a date
+    var todaysDateAndTimeObject = new Date();
+
+    // save date in ISO format / makee it a string then cut out time part
+    var todaysDateAndTime = todaysDateAndTimeObject.toISOString();
+    var todayDate = todaysDateAndTime.substring(0, 10);
+
+    // set vars to concat string based on todays date
+    var apiStartString = "https://statsapi.web.nhl.com/api/v1/schedule?teamId=23&startDate=2018-01-01&endDate=";
+    var concatApiString = apiStartString.concat(todayDate);
+    console.log(concatApiString);
+
+    var lastGames = new XMLHttpRequest();
+    lastGames.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            // parse response into empty object
+            var lastGameObject = {};
+            lastGameObject = JSON.parse(lastGames.response);
+
+
+            function createGameDate(game) {
+
+                // format date 
+                var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+                var gameDateFormatted = new Date(game.gameDate).toLocaleDateString('en', options);
+
+                // create paragraph elements
+                var liNode = document.createElement("p");
+                var liNodeFixture = document.createElement("p");
+
+                // create string to go into paragraph elements for both date and fixtures
+                var listTextStringDate = "";
+                listTextStringDate += gameDateFormatted;
+
+                var listTextStringFixture = "";
+                listTextStringFixture += game.teams.home.team.name;
+                listTextStringFixture += " vs ";
+                listTextStringFixture += game.teams.away.team.name;
+
+                // create text node with date and fixture strings
+                var liTextNodeDate = document.createTextNode(listTextStringDate);
+                var liTextNodeFixture = document.createTextNode(listTextStringFixture);
+
+                // add date and fixture text nodes to respect paragraph elements
+                liNode.appendChild(liTextNodeDate);
+                liNodeFixture.appendChild(liTextNodeFixture);
+
+                // create container div
+                var gameContainerNode = document.createElement('div');
+
+                // append paragraph elements to container div and add to page
+                gameContainerNode.appendChild(liNode);
+                gameContainerNode.appendChild(liNodeFixture);
+                document.getElementById('last-fixtures').appendChild(gameContainerNode);
+            }
+
+            // set game variables, run create game date function
+            var game1 = lastGameObject.dates["1"].games["0"];
+            var game2 = lastGameObject.dates["2"].games["0"];
+            var game3 = lastGameObject.dates["3"].games["0"];
+            var game4 = lastGameObject.dates["4"].games["0"];
+            var nextFourGames = [game1, game2, game3, game4];
+            nextFourGames.forEach(createGameDate);
+        }
+    };
+    lastGames.open("GET", concatApiString, true);
+    lastGames.send();
+}
+
+getLatestResults();
