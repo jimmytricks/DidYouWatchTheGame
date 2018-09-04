6cd9949739d7342158fc545b4d79cc956b6fcee6
James <jhixie4@gmail.com>
2018-09-04 11:42:08 -0700

    Switched to css VAR, added teamjs with hex colours

---
+++ JS/teams.js
@@ -0,0 +1,258 @@
+const teams = [
+    {
+        id: 24,
+        name: "Anaheim Ducks",
+        league: "nhl",
+        colours: {
+            hex: ["010101", "a2aaad", "fc4c02", "85714d"]
+        }
+    },
+    {
+        id: 53,
+        name: "Arizona Coyotes",
+        league: "nhl",
+        colours: {
+            hex: ["010101", "862633", "ddcba4"]
+        }
+    },
+    {
+        id: 6,
+        name: "Boston Bruins",
+        league: "nhl",
+        colours: {
+            hex: ["010101", "ffb81c"]
+        }
+    },
+    {
+        id: 7,
+        name: "Buffalo Sabres",
+        league: "nhl",
+        colours: {
+            hex: ["041e42", "a2aaad", "ffb81c", "c8102e"]
+        }
+    },
+    {
+        id: 20,
+        name: "Calgary Flames",
+        league: "nhl",
+        colours: {
+            hex: ["010101", "f1be48", "c8102e"]
+        }
+    },
+    {
+        id: 12,
+        name: "Carolina Hurricanes",
+        league: "nhl",
+        colours: {
+            hex: ["010101", "a2aaad", "c8102e"]
+        }
+    },
+    {
+        id: 16,
+        name: "Chicago Blackhawks",
+        league: "nhl",
+        colours: {
+            hex: [
+                "010101",
+                "ff671f",
+                "ffd100",
+                "001871",
+                "c8102e",
+                "00843d",
+                "cc8a00"
+            ]
+        }
+    },
+    {
+        id: 21,
+        name: "Colorado Avalanche",
+        league: "nhl",
+        colours: {
+            hex: ["010101", "236192", "a4a9ad", "6f263d"]
+        }
+    },
+    {
+        id: 29,
+        name: "Columbus Blue Jackets",
+        league: "nhl",
+        colours: {
+            hex: ["041e42", "a4a9ad", "c8102e"]
+        }
+    },
+    {
+        id: 25,
+        name: "Dallas Stars",
+        league: "nhl",
+        colours: {
+            hex: ["010101", "006341", "8a8d8f"]
+        }
+    },
+    {
+        id: 17,
+        name: "Detroit Red Wings",
+        league: "nhl",
+        colours: {
+            hex: ["c8102e"]
+        }
+    },
+    {
+        id: 22,
+        name: "Edmonton Oilers",
+        league: "nhl",
+        colours: {
+            hex: ["00205b", "cf4520"]
+        }
+    },
+    {
+        id: 13,
+        name: "Florida Panthers",
+        league: "nhl",
+        colours: {
+            hex: ["041e42", "b9975b", "c8102e"]
+        }
+    },
+    {
+        id: 26,
+        name: "Los Angeles Kings",
+        league: "nhl",
+        colours: {
+            hex: ["010101", "a2aaad"]
+        }
+    },
+    {
+        id: 30,
+        name: "Minnesota Wild",
+        league: "nhl",
+        colours: {
+            hex: ["154734", "ddcba4", "eaaa00", "a6192e"]
+        }
+    },
+    {
+        id: 8,
+        name: "Montreal Canadiens",
+        league: "nhl",
+        colours: {
+            hex: ["001e62", "a6192e"]
+        }
+    },
+    {
+        id: 18,
+        name: "Nashville Predators",
+        league: "nhl",
+        colours: {
+            hex: ["041e42", "ffb81c"]
+        }
+    },
+    {
+        id: 1,
+        name: "New Jersey Devils",
+        league: "nhl",
+        colours: {
+            hex: ["010101", "c8102e"]
+        }
+    },
+    {
+        id: 2,
+        name: "New York Islanders",
+        league: "nhl",
+        colours: {
+            hex: ["003087", "fc4c02"]
+        }
+    },
+    {
+        id: 3,
+        name: "New York Rangers",
+        league: "nhl",
+        colours: {
+            hex: ["0033a0", "c8102e"]
+        }
+    },
+    {
+        id: 9,
+        name: "Ottawa Senators",
+        league: "nhl",
+        colours: {
+            hex: ["010101", "c8102e", "c69214"]
+        }
+    },
+    {
+        id: 4,
+        name: "Philadelphia Flyers",
+        league: "nhl",
+        colours: {
+            hex: ["010101", "fa4616"]
+        }
+    },
+    {
+        id: 5,
+        name: "Pittsburgh Penguins",
+        league: "nhl",
+        colours: {
+            hex: ["010101", "ffb81c"]
+        }
+    },
+    {
+        id: 19,
+        name: "Saint Louis Blues",
+        league: "nhl",
+        colours: {
+            hex: ["041e42", "ffb81c", "003087"]
+        }
+    },
+    {
+        id: 28,
+        name: "San Jose Sharks",
+        league: "nhl",
+        colours: {
+            hex: ["010101", "e57200", "006272"]
+        }
+    },
+    {
+        id: 14,
+        name: "Tampa Bay Lightning",
+        league: "nhl",
+        colours: {
+            hex: ["00205b"]
+        }
+    },
+    {
+        id: 10,
+        name: "Toronto Maple Leafs",
+        league: "nhl",
+        colours: {
+            hex: ["00205b"]
+        }
+    },
+    {
+        id: 23,
+        name: "Vancouver Canucks",
+        league: "nhl",
+        colours: {
+            hex: ["00205b", "97999b", "041c2c"]
+        }
+    },
+    {
+        id: 54,
+        name: "Vegas Golden Knights",
+        league: "nhl",
+        colours: {
+            hex: ["010101", "b4975a", "333f42"]
+        }
+    },
+    {
+        id: 15,
+        name: "Washington Capitals",
+        league: "nhl",
+        colours: {
+            hex: ["041e42", "a2aaad", "782f40", "a6192e", "53565a"]
+        }
+    },
+    {
+        id: 52,
+        name: "Winnipeg Jets",
+        league: "nhl",
+        colours: {
+            hex: ["041e42", "c8102e"]
+        }
+    }
+];
